ce812817820a4269b981ff5d6b3dde485145c9f6
ifty03 <ashikulislamb84@gmail.com>
2022-04-26 21:00:04 +0600

    data loded from mongo db

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 /* all middleware */
 app.use(cors());
 app.use(express.json());
 
+/* http request */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tjsdk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  await client.connect();
+  const eventCollection = client.db("volunteerdb").collection("events");
+  try {
+    /* load data from database */
+    app.get("/events", async (req, res) => {
+      const query = {};
+      const cursor = eventCollection.find(query);
+      const events = await cursor.toArray();
+      res.send(events);
+    });
+    /* post data in data base */
+    app.post("/addEvent", (req, res) => {
+      const newEvent = req.body;
+      const event = eventCollection.insertOne(newEvent);
+      res.send(event);
+    });
+  } finally {
+    /*  await client.close(console.dir) */
+  }
+};
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
